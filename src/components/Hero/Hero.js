a970081dbdbaec50fbd05e3333cc7fe75e4521bb
TilakMaddy <tilak.madichetti@gmail.com>
2022-08-30 18:15:13 +0530

    initial commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,28 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcset="
+          /images/hero-img.avif 1x,
+          /images/hero-img@2x.avif 2x,
+          /images/hero-img@3x.avif 3x
+          "
+        />
+        <source
+          type="image/jpeg"
+          srcset="
+            /images/hero-img.jpg 1x,
+            /images/hero-img@2x.jpg 2x,
+            /images/hero-img@3x.jpg 3x
+          "
+        />
+        <HeroImage
+          alt=""
+          src="/images/hero-img.jpg"
+        />
+      </picture>
       <Swoop src="/swoop.svg" />
     </Wrapper>
   );
--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -5,9 +5,30 @@ const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+        <source
+          type="image/avif"
+          srcSet={`
+            ${src.replace('.jpg', '.avif')} 1x,
+            ${src.replace('.jpg', '@2x.avif')} 2x,
+            ${src.replace('.jpg', '@3x.avif')} 3x
+          `}
+        />
+        <source
+          type="image/jpeg"
+          srcSet={`
+            ${src} 1x,
+            ${src.replace('.jpg', '@2x.jpg')} 2x,
+            ${src.replace('.jpg', '@3x.jpg')} 3x
+          `}
+        />
+        <Image
+          alt=""
+          src={`${src}`}
+        />
+      </picture>
       </Anchor>
-      <Tags>
+      <Tags style={{ '--eachMax': (100 / tags.length) - 3 }}>
         {tags.map((tag) => (
           <Tag key={tag}>{tag}</Tag>
         ))}
@@ -28,20 +49,24 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+overflow: hidden;
+  text-overflow: ellipsis;
+  white-space:nowrap;
+  padding: 4px 0;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  margin-right: 8px;
 `;
 
 export default PhotoGridItem;
